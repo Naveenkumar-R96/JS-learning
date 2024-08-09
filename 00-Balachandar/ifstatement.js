f1c9462bba6d034959524362d3a7105518fff48d
NAVEEN KUMAR <nk946432@gmail.com>
2024-08-09 21:13:00 +0530

    if statement js

---
+++ 00-Balachandar/ifstatement.js
@@ -0,0 +1,17 @@
+
+
+
+let coustomerIsBanned=true;
+let reply;
+let viewer='hello'
+if(coustomerIsBanned='false'){
+  reply='sorry';
+}
+
+else if(viewer){
+  reply=`enjoy${viewer}`;
+}
+else{
+  reply='sorry guys';
+}
+console.log(reply);
