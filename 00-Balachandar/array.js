26e4e89c09adaf3cb117e961dd3289730115ff28
NAVEEN KUMAR <nk946432@gmail.com>
2024-08-11 17:39:51 +0530

    arrays

---
+++ 00-Balachandar/array.js
@@ -0,0 +1,40 @@
+/* let variable="abcded";
+console.log(variable.charAt(0));
+
+one=1
+two=2
+three=one+two
+console.log(three)
+
+let  myArray=["one, ",999,true]
+console.log(myArray);
+console.log(myArray.length)
+console.log(myArray[2])
+sua=myArray.pop();
+
+console.log(myArray);
+console.log(myArray[1])
+
+myArray.splice(1,0,"sub");
+console.log(myArray)
+
+
+let myarrayA=["false", 454,345,]
+console.log(myarrayA);
+
+const newArray=myArray.concat(myarrayA);
+
+console.log(newArray) */
+
+
+const fruit=["apple","orange","bannana","mango"];
+
+const things=["chair","bed","fridge", "stool"];
+
+const food=["dosa","parroto","noodles"]
+
+const add=[fruit,things,food];
+
+console.log(add);
+
+console.log(add[0][1],add[2][1]);
