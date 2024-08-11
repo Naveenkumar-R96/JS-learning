97960107c92528c7db5df5d0aca772c7aee1625a
NAVEEN KUMAR <nk946432@gmail.com>
2024-08-11 16:25:37 +0530

    function

---
+++ 00-Balachandar/function.js
@@ -0,0 +1,38 @@
+/* function sum (num,num1){
+   total=num+num1;
+   if (total==NaN){
+    num1=7;
+    return total;
+    
+   }
+   return total;
+}
+console.log(sum(2))*/ 
+
+//to get a email id without @ by using function
+
+/* 
+ let email="kjadjhfksjd@djfd";
+front="+91";
+back="9344049020";
+combaine=front+back;
+console.log(combaine)
+ */
+
+/* function toAdd(addingnum){
+
+   fron="+91 ";
+   combaine=fron+addingnum;
+   console.log(combaine);
+   return;
+
+}
+toAdd("395869458667"); */
+
+let name=(addingnum)=>{
+   fron="+91 ";
+   combaine=fron+addingnum;
+   console.log(combaine);
+   return;
+}
+name("984545454");
